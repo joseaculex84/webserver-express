0aa3a4135c012e4727d54ce3a0608e24591adf4b
“JoseDavid” <joseaculex84@gmail.com>
2020-06-01 23:25:58 -0400

    Primer Commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+//declara variable app con funcion express
+
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+//Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+
+app.get('/', (req, res) => {
+    //res.send('Hello World')
+    res.render('home', {
+        nombre: 'fsñklsdlkfs lsdkfsldkf lkfslksdf'
+    });
+});
+
+app.get('/about', (req, res) => {
+    //res.send('Hello World')
+    res.render('about');
+});
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${ port }`)
+});
